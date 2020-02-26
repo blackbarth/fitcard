c8b38b715c2ee86e46735b21d7f9a8ad47f14d14
Luis <blackbarth@outlook.com>
2020-02-26 09:34:40 -0300

    Atualização

--- FitCard.Web/wwwroot/js/site.js
+++ FitCard.Web/wwwroot/js/site.js
@@ -1,1 +1,21 @@
-﻿
+﻿//$(document).ready(function () {
+//    var usuario = new Object();
+//    usuario.email = "blackbarth@outlook.com";
+//    $.ajax({
+//        url: 'https://maximizi.com/fitcardapi/api/login',
+//        type: 'POST',
+//        contentType: 'application/json; charset=utf-8',
+//        dataType: 'json',
+//        data: usuario,
+//        success: function (data) {
+//            console.log(data); 
+//        },
+//        faiture: function (data, textStatus, xhr) {
+//            alert("falha");
+//            alert(data.responseText);
+//        },
+//        error: function (xhr, textStatus, errorThrown) {
+//            console.log('Error in Operation');  
+//        }
+//    });
+//});
